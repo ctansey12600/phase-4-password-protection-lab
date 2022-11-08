66f378b5d0f5c1d04e954dcf1dd1386b57b35809
Casey Tansey <ctansey12600@outlook.com>
2022-11-07 20:11:33 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   wrap_parameters format: []
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-  skip_before_action :authorized, only: [:show]
+  skip_before_action :authorized, only: [:create]
 
   def create
     user = User.create!(user_params)
+    session[:user_id] = user.id
     render json: user, status: :created
   end
 
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def render_unprocessable_entity(invalid)
-    render json: {error: invalid.record.errors}, status: :unprocessable_entity
+    render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
   
 end
